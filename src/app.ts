3fad2cb3730adff23c9da89176e5181b506ffec7
Nikolay Topkaridi <enemycnt@gmail.com>
2021-10-11 17:02:29 +0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express"
+import cors from "cors"
 import morgan from "morgan"
 import { routes } from "./routes"
 import { stream } from "./utils/logger"
 
 export const app: Application = express()
+app.use(cors())
 app.use(morgan("dev", { stream }))
 app.use(express.json())
 
