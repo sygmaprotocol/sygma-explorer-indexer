209a2e117589a7441febeaf44fd9c63dbfe0ae21
tcar <tcar121293@gmail.com>
2023-05-12 11:18:03 +0200

    refactor indexer logic;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- src/indexer/index.ts
+++ src/indexer/index.ts
@@ -1,44 +1,67 @@
-// @ts-nocheck
 import { PrismaClient } from "@prisma/client"
-import { EvmBridgeConfig, SygmaConfig } from "../sygmaTypes"
-import { indexDeposits, indexProposals, indexFailedHandlerExecutions } from "./indexer"
+import { testDomains, devDomains } from "./config"
+import { DomainTypes } from "./types"
+import { SubstrateIndexer } from "./services/substrateIndexer/substrateIndexer"
+import { EvmIndexer } from "./services/evmIndexer/evmIndexer"
 
-import {getSygmaConfig} from '../utils/getSygmaConfig'
-import { Config, IndexerSharedConfig } from "types"
-
-const prisma = new PrismaClient()
+enum Environment {
+  TESTNET = "testnet",
+  STAGE = "stage",
+}
 
 async function main() {
-  const sygmaconfig = await getSygmaConfig() as IndexerSharedConfig
-  
-  await prisma.$connect()
+  const prisma: PrismaClient = new PrismaClient()
+  const sharedConfig = await getSharedConfig(process.env.SHARED_CONFIG_URL || "https://config.develop.buildwithsygma.com/share/")
+  const localDomainsConfig = process.env.ENVIRONMENT == Environment.TESTNET ? testDomains : devDomains
 
-  const deleteTransfers = prisma.transfer.deleteMany()
+  for (const domain of sharedConfig.domains) {
+    const localdomain = localDomainsConfig.get(domain.id)
+    if (!localdomain) {
+      console.error("local domain is not defined for the domain: " + domain.id)
+      continue
+    }
 
-  await prisma.$transaction([deleteTransfers])
+    if (domain.type == DomainTypes.SUBSTRATE) {
+      const domainConfig = localDomainsConfig.get(domain.id)
+      if (!domainConfig) {
+        console.error("domain not configured in local config")
+        continue
+      }
+      try {
+        const substrateIndexer = new SubstrateIndexer(prisma, domainConfig, domain)
+        await substrateIndexer.init()
 
-  const evmBridges = sygmaconfig.chains.filter(
-    (c) => c.type !== "Substrate"
-  )
-  for (const bridge of evmBridges) {
-    await indexDeposits(bridge as Config, sygmaconfig)
-  }
-  console.log("\n***\n")
-  for (const bridge of evmBridges) {
-    await indexProposals(bridge as Config, sygmaconfig)
-  }
-  console.log("\n***\n")
-  for (const bridge of evmBridges) {
-    await indexFailedHandlerExecutions(bridge as Config, sygmaconfig)
+        const latestBlock = await substrateIndexer.indexPastEvents()
+        substrateIndexer.listenOnEvents(latestBlock)
+      } catch (err) {
+        console.error(`error on domain: ${domain.id}... skipping`)
+        continue
+      }
+    } else if (domain.type == DomainTypes.EVM) {
+      const domainConfig = localDomainsConfig.get(domain.id)
+      if (!domainConfig) {
+        console.error("domain not configured in local config")
+        continue
+      }
+      try {
+        const evmIndexer = new EvmIndexer(domainConfig, prisma, domain)
+        const latestBlock = await evmIndexer.indexPastEvents()
+        await evmIndexer.listenOnEvents(latestBlock)
+      } catch (err) {
+        console.error(`error on domain: ${domain.id}... skipping`)
+        continue
+      }
+    } else {
+      console.error("unsuported type: " + domain.type)
+    }
   }
 }
-main()
-  .catch((e) => {
-    console.error(e)
-    throw e
-  })
-  .finally(async() => {
-    await prisma.$disconnect()
-    console.log("\ndisconnect")
-    process.exit()
-  })
+
+async function getSharedConfig(url: string) {
+  const response = await fetch(url)
+  return await response.json()
+}
+
+main().catch(e => {
+  console.error(e)
+})
