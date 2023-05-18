5c2b2ea8642bfd5c54cb914f2775bcda70a3214b
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-05-18 17:33:20 -0400

    Pr review: last comments

--- src/types/SharedConfigTypes.ts
+++ src/types/SharedConfigTypes.ts
@@ -1,50 +1,50 @@
 export type SharedConfigResources = {
-  resourceId: string;
-  type: "erc20" | "erc721" | "permissionedGeneric" | "permissionlessGeneric";
-  address: string;
-  symbol: string;
-  decimals: number;
-};
-
-export type SharedConfigHandlers = {
-  type: "erc20" | "erc721" | "permissionedGeneric" | "permissionlessGeneric";
-  address: string;
-};
-
-export interface Domains<Type> {
-  id: number;
-  chainId: number;
-  name: string;
-  type: Type;
-  bridge: string;
-  nativeTokenSymbol: string;
-  nativeTokenFullName: string;
-  nativeTokenDecimals: number;
-  blockConfirmations: number;
-  startBlock: number;
-  resources: Array<SharedConfigResources>;
-};
-
-export interface EthereumSharedConfigDomain extends Domains<"ethereum"> {
-  handlers: Array<SharedConfigHandlers>;
-  feeRouter: string;
-  feeHandlers: Array<SharedConfigHandlers>;
+  resourceId: string
+  type: "erc20" | "erc721" | "permissionedGeneric" | "permissionlessGeneric"
+  address: string
+  symbol: string
+  decimals: number
 }
 
-export interface SubstrateSharedConfigDomain extends Domains<"substrate"> {
-  handlers: [];
-  feeRouter?: undefined;
-  feeHandlers?: null;
+export type SharedConfigHandlers = {
+  type: "erc20" | "erc721" | "permissionedGeneric" | "permissionlessGeneric"
+  address: string
+}
+
+export interface Domain<Type> {
+  id: number
+  chainId: number
+  name: string
+  type: Type
+  bridge: string
+  nativeTokenSymbol: string
+  nativeTokenFullName: string
+  nativeTokenDecimals: number
+  blockConfirmations: number
+  startBlock: number
+  resources: Array<SharedConfigResources>
+}
+
+export interface EthereumSharedConfigDomain extends Domain<"ethereum"> {
+  handlers: Array<SharedConfigHandlers>
+  feeRouter: string
+  feeHandlers: Array<SharedConfigHandlers>
+}
+
+export interface SubstrateDomain extends Domain<"substrate"> {
+  handlers: []
+  feeRouter?: undefined
+  feeHandlers?: null
 }
 
 export interface SharedConfigDomains {
-  domains: Array<EthereumSharedConfigDomain | SubstrateSharedConfigDomain>;
+  domains: Array<EthereumSharedConfigDomain | SubstrateDomain>
 }
 
-export interface SharedConfig extends Domains<"ethereum" | "substrate"> {
-  rpcUrl: string;
-};
+export interface SharedConfig extends Domain<"ethereum" | "substrate"> {
+  rpcUrl: string
+}
 
 export type ConfigError = {
-  error: { type: "config" | "shared-config"; message: string; name?: string };
-};
+  error: { type: "config" | "shared-config"; message: string; name?: string }
+}
