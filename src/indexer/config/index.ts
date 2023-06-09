43c792900fdd86f80e33a588ed05704f186d32bd
tcar <tcar121293@gmail.com>
2023-06-09 11:11:42 +0200

    load rpcUrl from env variable;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- src/indexer/config/index.ts
+++ src/indexer/config/index.ts
@@ -1,6 +1,3 @@
-import { testDomains } from "./testDomains"
-import { devDomains } from "./devDomains"
-
 export type LocalDomainConfig = {
   url: string
   startBlock: number
@@ -11,7 +8,7 @@ export enum Environment {
   STAGE = "devnet",
 }
 export const enum ResourceTypes {
-  FUNGIBLE = "FUNGIBLE",
+  FUNGIBLE = "fungible",
   NON_FUNGIBLE = "nonFungible",
   PERMISSIONED_GENERIC = "permissionedGeneric",
   PERMISSIONLESS_GENERIC = "permissionlessGeneric",
@@ -57,16 +54,42 @@ export type Resource = {
   decimals: number
 }
 
-export const getLocalConfig = (): Map<number, string> => {
-  return process.env.ENVIRONMENT == Environment.TESTNET ? testDomains : devDomains
-}
+export type RpcUrlConfig = Array<{
+  id: number
+  endpoint: string
+}>
 
 export const getSharedConfig = async (url: string): Promise<SharedConfig> => {
   try {
     const response = await fetch(url)
-    return await response.json()
+    return (await response.json()) as SharedConfig
   } catch (e) {
-    console.error(`Failed to fecth config for ${process.env.STAGE}`, e)
+    console.error(`Failed to fecth config for ${process.env.STAGE || ""}`, e)
     return Promise.reject(e)
   }
+}
+
+export const getSsmDomainConfig = (): Map<number, string> => {
+  const parsedResponse = JSON.parse(
+    process.env.RPC_URL_CONFIG ||
+      `[
+    {
+        "id": 1,
+        "endpoint": "https://eth-goerli.g.alchemy.com/v2/MQcsWqD94N1zldBRzG9XkdBM88LzgQLh"
+    },
+    {
+        "id": 2,
+        "endpoint": "https://eth-sepolia.g.alchemy.com/v2/M3RtUsouiiQ2KgADdSJq2pzWfhr4CXoW"
+    },
+    {
+        "id": 100,
+        "endpoint": "https://canto-testnet.plexnode.wtf"
+    }
+  ]`,
+  ) as RpcUrlConfig
+  const rpcUrlMap = new Map<number, string>()
+  for (const rpcConfig of parsedResponse) {
+    rpcUrlMap.set(rpcConfig.id, rpcConfig.endpoint)
+  }
+  return rpcUrlMap
 }
