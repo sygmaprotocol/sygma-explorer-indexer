d2b4ff139530bb45be9db98bd09700ee956ee6cb
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-06-16 10:47:11 -0400

    removing duplicated route

--- src/routes.ts
+++ src/routes.ts
@@ -20,10 +20,4 @@ export async function routes(fastify: FastifyInstance): Promise<void> {
     url: "/sender/:senderAddress/transfers",
     handler: TransfersController.transferBySender,
   })
-
-  fastify.route({
-    method: 'GET',
-    url: '/transfers/:id',
-    handler: TransfersController.transferById
-  })
 }
