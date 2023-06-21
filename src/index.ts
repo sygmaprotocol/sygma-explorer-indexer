6e1ead44e74873c85db0ebaa434a560f111f986f
Idris Isah <58168886+eedygreen@users.noreply.github.com>
2023-06-21 14:27:21 +0100

    changed host port (#59)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
 
-const PORT: number = Number(process.env.PORT!) || 3012
+const PORT: number = Number(process.env.PORT!) || 8000
 
 app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
