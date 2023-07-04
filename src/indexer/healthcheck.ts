cfc6f50add2d66204716430a5d69f7da4cbe7810
tcar <tcar121293@gmail.com>
2023-07-04 17:51:29 +0200

    add healthcheck route;
    Signed-off-by: tcar <tcar121293@gmail.com>

---
+++ src/indexer/healthcheck.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client"
+import fastifyHealthcheck from "fastify-healthcheck"
+import { fastify } from "fastify";
+
+// Create an instance of the Prisma client
+const prisma = new PrismaClient();
+export const healthcheckRoute = async function healthcheckRoute() {
+const app = fastify();
+
+const PORT: number = Number(process.env.PORT!) || 3000
+void app.register(fastifyHealthcheck, {
+    healthcheckUrl: "/health",
+    exposeUptime: true,
+    underPressureOptions: {
+    healthCheckInterval: 5000,
+        // eslint-disable-next-line @typescript-eslint/require-await
+        healthCheck: async () => {
+                try{
+                    await prisma.$connect()
+                    return true
+                }catch(err){
+                    return false
+                }
+            },
+        },
+    })
+
+
+    app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
+    if (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+    console.log(`⚡️[server]: Server is running at ${address}`)
+    })
+}
+
