71b4ccfb1cabb5263ce4953ce1590c03f7dc7562
Matija Petrunić <matija.petrunic@gmail.com>
2023-07-12 15:17:31 +0200

    chore: cleanup repo (#79)
    * Cleanup repo
    
    * Add code quality action
    
    * Move tests
    
    * Remove old tests
    
    * Update tsconfig.json
    
    * Remove root dir option
    
    * Rerun action on push to pr

--- src/indexer/healthcheck.ts
+++ src/indexer/healthcheck.ts
@@ -1,37 +1,35 @@
 import { PrismaClient } from "@prisma/client"
 import fastifyHealthcheck from "fastify-healthcheck"
-import { fastify } from "fastify";
+import { fastify } from "fastify"
 
 // Create an instance of the Prisma client
-const prisma = new PrismaClient();
-export const healthcheckRoute = async function healthcheckRoute() {
-const app = fastify();
+const prisma = new PrismaClient()
+export const healthcheckRoute = function healthcheckRoute(): void {
+  const app = fastify()
 
-const PORT: number = Number(process.env.PORT!) || 3000
-void app.register(fastifyHealthcheck, {
+  const PORT: number = Number(process.env.PORT!) || 3000
+  void app.register(fastifyHealthcheck, {
     healthcheckUrl: "/health",
     exposeUptime: true,
     underPressureOptions: {
-    healthCheckInterval: 5000,
-        // eslint-disable-next-line @typescript-eslint/require-await
-        healthCheck: async () => {
-                try{
-                    await prisma.$connect()
-                    return true
-                }catch(err){
-                    return false
-                }
-            },
-        },
-    })
+      healthCheckInterval: 5000,
+      // eslint-disable-next-line @typescript-eslint/require-await
+      healthCheck: async () => {
+        try {
+          await prisma.$connect()
+          return true
+        } catch (err) {
+          return false
+        }
+      },
+    },
+  })
 
-
-    app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
+  app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
     if (err) {
-        app.log.error(err)
-        process.exit(1)
+      app.log.error(err)
+      process.exit(1)
     }
     console.log(`⚡️[server]: Server is running at ${address}`)
-    })
+  })
 }
-
