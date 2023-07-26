a56b02e3ddde43df76f48d6d7139e01bb764e702
mj52951 <mj52951@fer.hr>
2023-07-26 13:21:59 +0200

    Added testing of transfer required fields

--- tests/e2e/indexing.spec.ts
+++ tests/e2e/indexing.spec.ts
@@ -7,15 +7,21 @@ const NUMBER_OF_TRANSFERS = 35;
 
 describe("Testing transfers", async function() {
     
-    it("Test number of transactions", async () => {
+    it("Test transfer count and required fields", async () => {
 
         const response = await axios.get("http://localhost:8000/api/transfers?page=1&limit=35")
-        const data = response.data as Array<any>
-
         expect(response.status).to.be.deep.equal(200)
-        expect(data.length).to.be.deep.equal(NUMBER_OF_TRANSFERS)        
-
+        
+        const transfers = response.data as Array<any>
+        expect(transfers.length).to.be.deep.equal(NUMBER_OF_TRANSFERS)
+        
+        transfers.map((transfer) => {
+            expect(transfer.id).to.be.not.null 
+            expect(transfer.depositNonce).to.be.not.null
+            expect(transfer.fromDomain).to.be.not.null 
+            expect(transfer.fromDomainId).to.be.not.null
+            expect(transfer.status).to.be.not.null
+        })
     }); 
-
-    it("Test")
+    
   })
