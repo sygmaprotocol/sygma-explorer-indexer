8f55ac8beaf77bf3e288efcacd1835de2ec0864f
tcar <tcar121293@gmail.com>
2023-08-01 13:18:25 +0200

    remove log formating;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,19 +1,9 @@
 import winston from "winston"
 import Transports from "winston-transport"
 
-const format = winston.format.printf(({ level, message, labels, timestamp, requestId }) => {
-  message = winston.format.colorize({ all: false, message: true }).colorize(level, message as string)
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  let log: string = `${timestamp as string} [${labels.module as string}] ${level.toUpperCase()}: ${message as string}`
-  if (requestId) {
-    log += `RequestId: ${requestId as string}`
-  }
-  return log
-})
-
 const transportsConfig: Transports[] = [
   new winston.transports.Console({
-    format: winston.format.combine(winston.format.timestamp(), winston.format.align(), format),
+    format: winston.format.combine(winston.format.timestamp(), winston.format.align()),
   }),
 ]
 
