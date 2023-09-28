cdbb7fb09d21397cc16cf0470a84d7d526543efe
mj52951 <mj52951@fer.hr>
2023-09-28 09:02:40 +0200

    Corrected import

--- src/indexer/services/monitoringService/index.ts
+++ src/indexer/services/monitoringService/index.ts
@@ -1,7 +1,7 @@
 import { CronJob } from "cron"
 import { DateTime } from "luxon"
 import { TransferStatus } from "@prisma/client"
-import { convertMillisecondsToMinutes } from "utils/helpers"
+import { convertMillisecondsToMinutes } from "../../../utils/helpers"
 import TransferRepository from "../../repository/transfer"
 import { logger } from "../../../utils/logger"
 import { NotificationSender } from "./notificationSender"
@@ -17,7 +17,6 @@ export async function checkTransferStatus(transferRepository: TransferRepository
   for (const transfer of transfers) {
     const duration = Date.now() - transfer.timestamp.getTime()
     const durationInMins = convertMillisecondsToMinutes(duration)
-
     if (durationInMins > INCIDENT_TIME) {
       await notificationSender.sendNotification({ Message: "Incident" })
       logger.info("Incident sent")
