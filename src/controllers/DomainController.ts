66b3de183042a173dbfde8cf23c308ce5ac87775
MakMuftic <makmuftic@chainsafe.io>
2024-02-12 14:25:00 +0100

    resolve comment on default error msg

--- src/controllers/DomainController.ts
+++ src/controllers/DomainController.ts
@@ -7,14 +7,14 @@ import { Environment } from "@buildwithsygma/sygma-sdk-core"
 import { logger } from "../utils/logger"
 import { DomainMetadataConfig } from "../utils/domainMetadata"
 
-const env = (process.env.ENV as Environment) || Environment.TESTNET
+const env = (process.env.ENV as Environment) || Environment.MAINNET
 export const DomainsController = {
   domains: function (request: FastifyRequest<{}>, reply: FastifyReply): void {
     const metadata = DomainMetadataConfig[env]
     if (metadata) {
       void reply.status(200).send(metadata)
     } else {
-      logger.error("Unable to find metadata for .")
+      logger.error(`Unable to find metadata for environment ${env}`)
       void reply.status(500)
     }
   },
