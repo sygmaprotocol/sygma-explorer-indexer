bec717a9903fdcc25a9078d1662b28e40f24217e
mj52951 <matija.jelavic@chainsafe.io>
2024-09-09 11:43:20 +0200

    License and workflow changes

--- src/indexer/utils/bitcoin/index.ts
+++ src/indexer/utils/bitcoin/index.ts
@@ -1,3 +1,7 @@
+/*
+The Licensed Work is (c) 2023 Sygma
+SPDX-License-Identifier: LGPL-3.0-only
+*/
 import FeeRepository from "indexer/repository/fee"
 import { sha256 } from "ethers"
 import { BigNumber } from "@ethersproject/bignumber"
