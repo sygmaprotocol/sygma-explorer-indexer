f1195e76df27972b1c49857e78c9be8974e99fcd
tcar121293 <tcar121293@gmail.com>
2025-01-10 15:05:57 +0100

    chore: remove khala routes and metadata (#239)

--- src/routes/routes.mainnet.ts
+++ src/routes/routes.mainnet.ts
@@ -13,10 +13,8 @@ export const routesMainnet: Map<string, Route[]> = new Map([
       { fromDomainId: "1", toDomainId: "7", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000000", type: "gmp" },
       { fromDomainId: "1", toDomainId: "5", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000000", type: "gmp" },
       { fromDomainId: "1", toDomainId: "3", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000001", type: "fungible" },
-      { fromDomainId: "1", toDomainId: "2", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000001", type: "fungible" },
     ],
   ],
-  ["2", [{ fromDomainId: "2", toDomainId: "1", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000001", type: "fungible" }]],
   ["3", [{ fromDomainId: "3", toDomainId: "1", resourceId: "0x0000000000000000000000000000000000000000000000000000000000000001", type: "fungible" }]],
   [
     "4",
